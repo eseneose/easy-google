740683ea2406a3a2a623f3ea155f2ce52105bf0e
eseneose <eseneose96@gmail.com>
2022-02-26 03:31:41 +0100

    built frontend homepage

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    mode: 'jit',
+    purge: [
+        './pages/**/*.{js,jsx,ts,tsx,vue}',
+        './components/**/*.{js,jsx,ts,tsx,vue}',
+    ],
+
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
